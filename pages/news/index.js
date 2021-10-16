32af9df3ba977259fd7d2302f35d25e9818ca527
Alejandro <tk.jalejandro@gmail.com>
2021-10-16 11:15:27 +0200

    Learned how to link pages and the difference between using A tags and Link from next/link components.

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,9 +1,18 @@
 //No need to import react from react
 
+import Link from "next/link"
+
 const NewsPage = () => {
 
     return (
+        <>
         <h1>News Page</h1>
+        <ul>
+            <li><Link href="/news/next-js-is-the-best">Next JS is the  best</Link></li>
+            <li><Link href="/news/learn-react">Learn React</Link></li>
+        </ul>
+        </>
+        
     )
 }
 
