4e39a2c82d26fe74f3734bc151653fc469b2c7b7
Alejandro <tk.jalejandro@gmail.com>
2021-10-16 10:55:04 +0200

    Learned the basics of extracting dynamic route data.

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -3,7 +3,19 @@
 //                3
 //                4
 
+import { useRouter } from "next/dist/client/router"
+
 const DetailPage = () => {
+
+    const router = useRouter()
+    //This way he can identify what. Try to console it.
+    
+    //console.log(router.query.newsId)
+
+
+    const newsId = router.query.newsId
+    //Send request to API!
+    //To fetch the news item with  news ID
 
     return (
         <h1>Detail Page</h1>
