543722f621d09af9aa138e75b4fdca10469b6133
Alejandro <tk.jalejandro@gmail.com>
2021-10-16 10:41:15 +0200

    Learned creating dynamic routing.

---
+++ pages/news/[newsId].js
@@ -0,0 +1,13 @@
+//Our domain/news/1
+//                2
+//                3
+//                4
+
+const DetailPage = () => {
+
+    return (
+        <h1>Detail Page</h1>
+    )
+}
+
+export default DetailPage
